eed7208aa22d3ab90f215815eb46e8dc456c561e
Garrett Gurnett <garrettgurnett@gmail.com>
2017-08-28 15:58:28 -0600

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class RegistrationForm extends React.Component{
+	render(){
+		return(
+			<form>
+			<label>Username</label>
+				<input type="text" />
+				<input type="password" />
+				<button type="submit"/>
+			</form>
+			
+		);
+	}
+}
+
